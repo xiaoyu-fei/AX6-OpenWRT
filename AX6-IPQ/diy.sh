d4a3911b4e3d747fdfe9a09d2b7c8fed0c689ed5
xiaoyu-fei <xiaoyufe@gmail.com>
2026-01-16 13:21:05 +0800

    2

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -53,3 +53,8 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 
 #修改主机名
 #sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
+
+# Copy dae related packages
+cp -rf $GITHUB_WORKSPACE/package/dae ./package/
+cp -rf $GITHUB_WORKSPACE/package/luci-app-dae ./package/
+cp -rf $GITHUB_WORKSPACE/package/v2ray-geodata ./package/
