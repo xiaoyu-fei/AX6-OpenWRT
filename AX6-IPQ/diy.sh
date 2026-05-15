fcbf1e769a491354f691e4f2ea8e4463c74ffafd
xiaoyu-fei <xiaoyufe@gmail.com>
2026-05-15 08:27:20 +0800

    build(AX6-NSS): 调整默认IP并添加daed插件支持
    1.  修改OpenWrt默认管理IP为10.0.1.1
    2.  在CI流程中添加clang、llvm、npm依赖并安装pnpm
    3.  克隆luci-app-daed插件并添加所需内核配置项
    4.  更新README中的默认IP信息

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -49,7 +49,7 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 
 
 #修改默认IP
-#sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.1.1/g' package/base-files/files/bin/config_generate
 
 #修改主机名
 #sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
